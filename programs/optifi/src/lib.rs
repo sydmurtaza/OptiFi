c8437fdc75b28512547a36d185a2c0a72213ac09
Syed Hussain Murtaza <i221513@nu.edu.pk>
2025-04-22 00:33:54 +0500

    Updating program

--- programs/optifi/src/lib.rs
+++ programs/optifi/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program;
 
 declare_id!("7HSCEYkgef7SnXk9DoZ4rEh5Qg5yvccpidCxfzcm7wMf");
 
@@ -6,11 +7,36 @@ declare_id!("7HSCEYkgef7SnXk9DoZ4rEh5Qg5yvccpidCxfzcm7wMf");
 pub mod optifi {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    #[account]
+    pub struct YieldPool {
+        pub pool_id: String,
+        pub total_deposits: u64,
+        pub allocations: Vec<Allocation>,
+        pub last_rebalance: i64,
+    }
+
+    #[derive(AnchorSerialize, AnchorDeserialize, Clone)]
+    pub struct Allocation {
+        pub protocol: String,
+        pub percentage: u64,
+        pub apy: f64,
+    }
+
+    pub fn create_pool(ctx: Context<CreatePool>, pool_id: String) -> Result<()> {
+        let pool = &mut ctx.accounts.pool;
+        pool.pool_id = pool_id;
+        pool.total_deposits = 0;
+        pool.allocations = vec![];
+        pool.last_rebalance = Clock::get()?.unix_timestamp;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreatePool<'info> {
+    #[account(init, payer = user, space = 8 + 32 + 8 + 200 + 8)]
+    pub pool: Account<'info, YieldPool>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
